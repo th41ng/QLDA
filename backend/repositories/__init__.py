b572523cfb380150e021fb09145eeb5baceb4e6e
th41ng <nguyenlethanhthang@gmail.com>
2026-04-14 21:21:48 +0700

    fix: Loi trong backend repo

--- backend/repositories/__init__.py
+++ backend/repositories/__init__.py
@@ -1,37 +1,83 @@
+# from .companies import get_company_by_user_id
+# from .jobs import apply_tags, create_job_record, delete_job_record, get_job_by_id, list_jobs, list_jobs_for_recruiter, list_published_jobs
+# from .profiles import get_profile_by_user_id
+# from .tags import list_active_categories, list_active_tags
+# from .users import get_user_by_email, get_user_by_id
+
+# __all__ = [
+#     "apply_tags",
+#     "create_job_record",
+#     "delete_job_record",
+# from .cv_templates import count_active_cv_templates, list_active_cv_templates
+# from .profiles import get_profile_by_user_id
+# from .resumes import get_resume_by_id, list_resumes_by_user_id, list_screenable_resumes
+# from .statistics import count_active_categories, count_cv_templates, count_published_employers, count_published_jobs
+# from .users import get_user_by_email, get_user_by_id
+
+# __all__ = [
+#     "count_active_categories",
+#     "count_active_cv_templates",
+#     "count_cv_templates",
+#     "count_published_employers",
+#     "count_published_jobs",
+#     "get_company_by_user_id",
+#     "get_job_by_id",
+#     "get_profile_by_user_id",
+#     "get_resume_by_id",
+#     "get_user_by_email",
+#     "get_user_by_id",
+#     "list_active_categories",
+#     "list_active_tags",
+#     "list_jobs",
+#     "list_jobs_for_recruiter",
+#     "list_published_jobs",
+#     "list_active_cv_templates",
+#     "list_resumes_by_user_id",
+#     "list_screenable_resumes",
+# ]
 from .companies import get_company_by_user_id
-from .jobs import apply_tags, create_job_record, delete_job_record, get_job_by_id, list_jobs, list_jobs_for_recruiter, list_published_jobs
+from .jobs import (
+    apply_tags,
+    create_job_record,
+    delete_job_record,
+    get_job_by_id,
+    list_jobs,
+    list_jobs_for_recruiter,
+    list_published_jobs,
+)
 from .profiles import get_profile_by_user_id
 from .tags import list_active_categories, list_active_tags
 from .users import get_user_by_email, get_user_by_id
+from .cv_templates import count_active_cv_templates, list_active_cv_templates
+from .resumes import get_resume_by_id, list_resumes_by_user_id, list_screenable_resumes
+from .statistics import (
+    count_active_categories,
+    count_cv_templates,
+    count_published_employers,
+    count_published_jobs,
+)
 
 __all__ = [
     "apply_tags",
     "create_job_record",
     "delete_job_record",
-from .cv_templates import count_active_cv_templates, list_active_cv_templates
-from .profiles import get_profile_by_user_id
-from .resumes import get_resume_by_id, list_resumes_by_user_id, list_screenable_resumes
-from .statistics import count_active_categories, count_cv_templates, count_published_employers, count_published_jobs
-from .users import get_user_by_email, get_user_by_id
-
-__all__ = [
+    "get_job_by_id",
+    "list_jobs",
+    "list_jobs_for_recruiter",
+    "list_published_jobs",
+    "get_company_by_user_id",
+    "get_profile_by_user_id",
+    "get_user_by_email",
+    "get_user_by_id",
+    "list_active_categories",
+    "list_active_tags",
     "count_active_categories",
     "count_active_cv_templates",
     "count_cv_templates",
     "count_published_employers",
     "count_published_jobs",
-    "get_company_by_user_id",
-    "get_job_by_id",
-    "get_profile_by_user_id",
-    "get_resume_by_id",
-    "get_user_by_email",
-    "get_user_by_id",
-    "list_active_categories",
-    "list_active_tags",
-    "list_jobs",
-    "list_jobs_for_recruiter",
-    "list_published_jobs",
     "list_active_cv_templates",
+    "get_resume_by_id",
     "list_resumes_by_user_id",
     "list_screenable_resumes",
 ]
